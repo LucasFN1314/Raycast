a96b4049a3951aba0329177bb54e00a075637957
LucasFN1314 <lmartinez@trememote.com.ar>
2023-03-19 20:52:10 -0300

    Raycast.js

---
+++ raycast.js
@@ -0,0 +1,57 @@
+class Raycast {
+    constructor() {
+        this.MAP_SIZE = 16;
+        this.MAP_SCALE = 10; // || Escalado de cada punto del mapa
+        this.MAP_SPEED = (this.MAP_SCALE / 2) / 10;
+        this.MAP_RANGE = this.MAP_SIZE * this.MAP_SCALE;
+
+        this.TILES_COLORS = {
+            0: '#aaa',
+            1: '#555'
+        };
+
+        this.MAP = [
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1,
+            1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        ];
+    }
+    render() {
+        this.minimap();
+    }
+    update() {
+
+    }
+
+    minimap() {
+        for (let row = 0; row < this.MAP_SIZE; row++) {
+            for (let col = 0; col < this.MAP_SIZE; col++) {
+                let tile = row * this.MAP_SIZE + col;
+                drawRect({
+                    color: this.tileColor(this.MAP[tile]),
+                    x: col * this.MAP_SCALE,
+                    y: row * this.MAP_SCALE,
+                    width: this.MAP_SCALE,
+                    height: this.MAP_SCALE
+                });
+            }
+        }
+    }
+
+    tileColor(index) {
+        return this.TILES_COLORS[index] ??= "white";
+    }
+}
