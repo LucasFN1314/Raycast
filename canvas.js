607ecebff33da5f02db239343e1ee9100e86fea4
LucasFN1314 <lmartinez@trememote.com.ar>
2023-03-19 19:57:11 -0300

    Canvas.js | Half Size

--- canvas.js
+++ canvas.js
@@ -1,4 +1,6 @@
 let canvas, context = null;
+let halfwidth, halfHeight;
+
 function setCanvas(identifier) {
     canvas = getElement(identifier);
     context = canvas.getContext("2d");
@@ -14,6 +16,9 @@ function resize() {
     let winSize = windowSize();
     canvas.width = winSize.width;
     canvas.height = winSize.height;
+
+    halfwidth = winSize.width/2;
+    halfHeight = winSize.height/2;
 }
 
 function drawRect(properties) {
